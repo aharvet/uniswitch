25597681cec1122fa6c801d7e7e6a4aa20783b87
Alexandre <alexandrewlff@gmail.com>
2022-06-23 17:55:34 +0200

    Change contracts folders

--- contracts/UniswitchPool.sol
+++ contracts/UniswitchPool.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
-import "./IUniswitchFactory.sol";
+import "./interfaces/IUniswitchFactory.sol";
 
 import "hardhat/console.sol";
 
