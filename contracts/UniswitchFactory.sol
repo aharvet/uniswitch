8e72c43bcd40aedd720377b39ed9adac7ea908a7
Alexandre <alexandrewlff@gmail.com>
2022-07-12 22:06:11 +0200

    Update factory interface

--- contracts/UniswitchFactory.sol
+++ contracts/UniswitchFactory.sol
@@ -2,15 +2,14 @@
 
 pragma solidity 0.6.12;
 
+import {IUniswitchFactory} from "./interfaces/IUniswitchFactory.sol";
 import {UniswitchPool} from "./UniswitchPool.sol";
 
-contract UniswitchFactory {
-    mapping(address => address) public tokenToPool;
-    mapping(address => address) public poolToToken;
+contract UniswitchFactory is IUniswitchFactory {
+    mapping(address => address) public override tokenToPool;
+    mapping(address => address) public override poolToToken;
 
-    event PoolLaunched(address indexed token, address pool);
-
-    function launchPool(address token) external {
+    function launchPool(address token) external override {
         require(
             tokenToPool[token] == address(0),
             "UniswitchFactory: pool already created for token"
--- contracts/interfaces/IUniswitchFactory.sol
+++ contracts/interfaces/IUniswitchFactory.sol
@@ -3,17 +3,11 @@
 pragma solidity 0.6.12;
 
 interface IUniswitchFactory {
-    function poolsAmount() external view returns (uint256);
+    event PoolLaunched(address indexed token, address pool);
 
-    function tokens(uint256 i) external view returns (address);
+    function tokenToPool(address token) external view returns (address);
 
-    function tokenToPool(address _addr) external view returns (address);
+    function poolToToken(address pool) external view returns (address);
 
-    function poolToToken(address _addr) external view returns (address);
-
-    event PoolLaunched(address token, address pool);
-
-    function launchPool(address _token) external;
-
-    function getTokens() external view returns (address[] memory);
+    function launchPool(address token) external;
 }
